a1b9aeaa3855c05a60cabf418206d56afe83770c
Jeremy Aalders <jdaalders@gmail.com>
2024-02-15 16:35:47 -0400

    Fix column classes for template offerings.

---
+++ src/components/MenuComponents/Templates/MenuSectionOptions.vue
@@ -0,0 +1,75 @@
+<template>
+  <q-dialog>
+    <q-card style="width: 700px; max-width: 80vw">
+      <q-card-section>
+        <div class="col-3">
+          <div class="section-layout">
+            <span class="section-layout-column" style="flex: 0 0 auto; width: 100%"></span>
+          </div>
+        </div>
+        <div class="col-3">
+          <div class="section-layout">
+            <span class="section-layout-column" style="flex: 0 0 auto; width: 50%"></span>
+            <span class="section-layout-column" style="flex: 0 0 auto; width: 50%"></span>
+          </div>
+        </div>
+        <div class="col-3">
+          <div class="section-layout">
+            <span class="section-layout-column" style="flex: 0 0 auto; width: 33.33%"></span>
+            <span class="section-layout-column" style="flex: 0 0 auto; width: 33.33%"></span>
+            <span class="section-layout-column" style="flex: 0 0 auto; width: 33.33%"></span>
+          </div>
+        </div>
+        <div class="col-3">
+          <div class="section-layout">
+            <span class="section-layout-column" style="flex: 0 0 auto; width: 25%"></span>
+            <span class="section-layout-column" style="flex: 0 0 auto; width: 25%"></span>
+            <span class="section-layout-column" style="flex: 0 0 auto; width: 25%"></span>
+            <span class="section-layout-column" style="flex: 0 0 auto; width: 25%"></span>
+          </div>
+        </div>
+      </q-card-section>
+
+      <q-card-section>
+        <div class="col-3">
+          <div class="section-layout">
+            <span class="section-layout-column" style="flex: 0 0 auto; width: 25%"></span>
+            <span class="section-layout-column" style="flex: 0 0 auto; width: 25%"></span>
+            <span class="section-layout-column" style="flex: 0 0 auto; width: 50%"></span>
+          </div>
+        </div>
+        <div class="col-3">
+          <div class="section-layout">
+            <span class="section-layout-column" style="flex: 0 0 auto; width: 50%"></span>
+            <span class="section-layout-column" style="flex: 0 0 auto; width: 25%"></span>
+            <span class="section-layout-column" style="flex: 0 0 auto; width: 25%"></span>
+          </div>
+        </div>
+        <div class="col-3">
+          <div class="section-layout">
+            <span class="section-layout-column" style="flex: 0 0 auto; width: 66.66%"></span>
+            <span class="section-layout-column" style="flex: 0 0 auto; width: 33.33%"></span>
+          </div>
+        </div>
+        <div class="col-3">
+          <div class="section-layout">
+            <span class="section-layout-column" style="flex: 0 0 auto; width: 66.66%"></span>
+            <span class="section-layout-column" style="flex: 0 0 auto; width: 33.33%"></span>
+          </div>
+        </div>
+      </q-card-section>
+    </q-card>
+  </q-dialog>
+</template>
+
+<style>
+.section-layout {
+  display: flex;
+  height: 70px;
+  padding: 10px;
+}
+
+.section-layout-column {
+  background-color: #d5dadf;
+}
+</style>
